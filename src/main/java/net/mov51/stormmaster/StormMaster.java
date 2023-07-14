e1edb03ff2b596eb2a97aba27e863ff00550f42a
mov <mov51@mov51.net>
2023-07-14 12:09:41 -0400

    IT WORKS

--- src/main/java/net/mov51/stormmaster/StormMaster.java
+++ src/main/java/net/mov51/stormmaster/StormMaster.java
@@ -1,17 +1,29 @@
 package net.mov51.stormmaster;
 
+import net.mov51.stormmaster.events.ProjectileHitEventListener;
+import org.bukkit.World;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class StormMaster extends JavaPlugin {
+    public static Plugin plugin;
+    public static int thunderDuration = 0;
+    public static World thunderWorld;
 
     @Override
     public void onEnable() {
+        plugin = this;
+        this.saveConfig();
+        thunderDuration = this.getConfig().getInt("thunder-duration");
         // Plugin startup logic
-
+        this.getLogger().info("StormMaster is now enabled!");
+        //register event
+        getServer().getPluginManager().registerEvents(new ProjectileHitEventListener(), this);
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        this.getConfig().set("weather-duration", thunderDuration);
     }
 }
---
+++ src/main/java/net/mov51/stormmaster/events/ProjectileHitEventListener.java
@@ -0,0 +1,41 @@
+package net.mov51.stormmaster.events;
+
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Trident;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.ProjectileHitEvent;
+
+public class ProjectileHitEventListener implements Listener {
+    @EventHandler
+    public void onProjectileHitEvent(ProjectileHitEvent e) {
+        if(e.getEntity().getType() != org.bukkit.entity.EntityType.TRIDENT){
+            return;
+        }
+        if(!(((Trident) e.getEntity()).getItem().getEnchantments().containsKey(Enchantment.CHANNELING))){
+            return;
+        }
+        if(e.getHitBlock() == null){
+            return;
+        }
+        if(e.getHitBlock().getType() != Material.DIAMOND_BLOCK){
+            return;
+        }
+        for(Entity en: e.getEntity().getNearbyEntities(1.5,1.5,1.5)){
+            if(en.getType() != org.bukkit.entity.EntityType.DROPPED_ITEM){
+                continue;
+            }
+            if(((org.bukkit.entity.Item) en).getItemStack().getType() == org.bukkit.Material.NETHER_STAR){
+                en.getWorld().strikeLightning(en.getLocation());
+                en.getWorld().setStorm(true);
+                en.getWorld().setThundering(true);
+                en.getWorld().setThunderDuration(en.getWorld().getThunderDuration() + 24000);
+                en.remove();
+                e.getHitBlock().setType(Material.COAL_BLOCK);
+                break;
+            }
+        }
+    }
+}
